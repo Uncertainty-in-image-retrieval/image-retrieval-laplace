2d785cb66cf27e12146eaf16eb4794a9806372fd
mapl <placenti.marco@gmail.com>
2022-02-22 13:59:18 +0100

    added setup.py and init in src

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='02460 Project',
+    author='Uncertainty in Image Retrieval Group',
+    license='',
+)
