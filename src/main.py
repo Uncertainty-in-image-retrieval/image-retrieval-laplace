5dfadb196e9fb97faed506dce59eab4ce2de3a9c
mapl <placenti.marco@gmail.com>
2022-03-03 16:07:46 +0100

    fixed fucking space error

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def train(model, loss_func, mining_func, train_loader, optimizer, epoch):
         optimizer.step()
         wandb.log({"loss": loss, "mined triples": mining_func.num_triplets})
         if batch_idx % 20 == 0:
-             if TRAINING_HP['miner'] == 'TripletMarginMiner':
+            if TRAINING_HP['miner'] == 'TripletMarginMiner':
                 print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}, Number of mined triplets = {mining_func.num_triplets}")
             elif TRAINING_HP['miner'] == 'BatchEasyHardMiner':
                 print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}")
