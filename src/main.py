b614872aab98a2b33ed38294e462f387562cd8fe
mapl <placenti.marco@gmail.com>
2022-03-10 15:09:59 +0100

    working code yay

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.distributions as dists
 import torch.optim as optim
 from torchvision import transforms
@@ -7,7 +8,7 @@ from pytorch_metric_learning import testers
 from pytorch_metric_learning.utils.accuracy_calculator import AccuracyCalculator
 
 from src.data.make_dataset import get_data
-from src.models.model import Net
+from src.models.model import Net, NetSoftmax
 from src.utils.pytorch_metric_learning import setup_pytorch_metric_learning
 from src.utils.plots import plot_umap
 
@@ -20,27 +21,30 @@ from laplace import Laplace
 from netcal.metrics import ECE
 
 
-def train(model, loss_func, mining_func, train_loader, optimizer, epoch, device, laplace=False):
+def train(model, loss_func, mining_func, train_loader, optimizer, epoch, device):
     
     for batch_idx, (data, labels) in enumerate(train_loader):
         data, labels = data.to(device), labels.to(device)
-        if laplace:
-            pass # TODO: model.fit(train_loader[batch_idx])
-        else:
-            model.train()
-            optimizer.zero_grad()
-            embeddings = model(data)
+        
+        model.train()
+        optimizer.zero_grad()
+        embeddings = model(data)
+
+        if mining_func:
             indices_tuple = mining_func(embeddings, labels)
             loss = loss_func(embeddings, labels, indices_tuple)
-            loss.backward()
-            optimizer.step()
-            wandb.log({"loss": loss})
-            if batch_idx % 20 == 0:
-                if TRAINING_HP['miner'] == 'TripletMarginMiner':
-                    print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}, Number of mined triplets = {mining_func.num_triplets}")
-                    wandb.log({"mined triples": mining_func.num_triplets})
-                elif TRAINING_HP['miner'] == 'BatchEasyHardMiner':
-                    print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}")
+        else:
+            loss = loss_func(embeddings, labels)
+
+        loss.backward()
+        optimizer.step()
+        #wandb.log({"loss": loss})
+        if batch_idx % 20 == 0:
+            if TRAINING_HP['miner'] == 'TripletMarginMiner':
+                print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}, Number of mined triplets = {mining_func.num_triplets}")
+                wandb.log({"mined triples": mining_func.num_triplets})
+            elif TRAINING_HP['miner'] == 'BatchEasyHardMiner':
+                print(f"Epoch {epoch} Iteration {batch_idx}/{len(train_loader)}: Loss = {loss}")
         
         
 def get_all_embeddings(dataset, model):
@@ -57,24 +61,24 @@ def knn(train_embeddings, train_labels, test_embeddings, test_labels):
 
 
 def test(model, test_loader, device, laplace=False):
-    if not laplace:
-        acc_map = []
-        model.eval()
-        targets = torch.cat([y for x, y in test_loader], dim=0).numpy()
-        for batch_idx, (data, labels) in enumerate(test_loader):
-            data, labels = data.to(device), labels.to(device)
+    acc_map = []
+    #model.eval()
+    targets = torch.cat([y for x, y in test_loader], dim=0).numpy()
+    for batch_idx, (data, labels) in enumerate(test_loader):
+        data, labels = data.to(device), labels.to(device)
+        if laplace:
+            embeddings = model(data)
+        else:
             embeddings = torch.softmax(model(data), dim=1)
-            probs_map = torch.cat([embeddings], dim=1).detach().numpy()
-            y_hat = probs_map.argmax(-1)
-            acc_map.extend([1 if y_hat[idx] == labels[idx] else 0 for idx in range(len(y_hat))])
-            ece_map = ECE(bins=10).measure(probs_map, labels.detach().numpy())
-            nll_map = dists.Categorical(torch.tensor(probs_map)).log_prob(labels).mean()
-    
-        print(f'[MAP] Acc.: {sum(acc_map)/len(acc_map)}; ECE: {ece_map}; NLL: {nll_map}')
-        wandb.log({"Accuracy": sum(acc_map)/len(acc_map), "ECE": ece_map, "NLL": nll_map})
+        probs_map = torch.cat([embeddings], dim=1).detach().numpy()
+        y_hat = probs_map.argmax(-1)
+        acc_map.extend([1 if y_hat[idx] == labels[idx] else 0 for idx in range(len(y_hat))])
+        ece_map = ECE(bins=10).measure(probs_map, labels.detach().numpy())
+        nll_map = dists.Categorical(torch.tensor(probs_map)).log_prob(labels).mean()
 
-    if laplace:
-        pass# TODO: implement
+    print(f'[MAP] Acc.: {sum(acc_map)/len(acc_map)}; ECE: {ece_map}; NLL: {nll_map}')
+    #wandb.log({"Accuracy": sum(acc_map)/len(acc_map), "ECE": ece_map, "NLL": nll_map})
+
         
     
     
@@ -114,8 +118,8 @@ def reshuffle_train(training_data):
 
 def run():
 
-    wandb.login(key=WANDB_KEY)
-    wandb.init(project=PROJECT['name'], name=PROJECT['experiment'], config=TRAINING_HP)
+    #wandb.login(key=WANDB_KEY)
+    #wandb.init(project=PROJECT['name'], name=PROJECT['experiment'], config=TRAINING_HP)
 
     train_loader, test_loader, train_data, test_data = preproc_data()
 
@@ -125,49 +129,62 @@ def run():
         device = torch.device("cpu")
 
     model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=TRAINING_HP['lr'])
+    optimizer_model = optim.Adam(model.parameters(), lr=TRAINING_HP['lr'])
+    model_softmax = NetSoftmax().to(device)
+    optimizer_model_softmax = optim.Adam(model_softmax.parameters(), lr=TRAINING_HP['lr'])
+    
+    
 
     loss_func, mining_func = setup_pytorch_metric_learning(TRAINING_HP)
 
+    cross_entropy_loss = nn.CrossEntropyLoss()
     for epoch in range(1, TRAINING_HP['epochs'] + 1):
         
         ### TRAINING ###
-        print("Training without Laplace approximation...")
-        train(model, loss_func, mining_func, train_loader, optimizer, epoch, device, laplace=False)
+        #print("Training Siamese without Laplace approximation...")
+        #train(model, loss_func, mining_func, train_loader, optimizer, epoch, device, laplace=False)
 
-        
-        #print("Training with Laplace approximation...") # VERY SLOW
-        #la = Laplace(model, 'classification',
-        #     subset_of_weights='last_layer',
-        #     hessian_structure='kron')
-        #model_la = train(la, loss_func, mining_func, test_loader, optimizer, epoch, laplace=True)
-        #la.optimize_prior_precision(method='marglik')
+        print("Training Traditional without Laplace approximation...")
+        train(model_softmax, cross_entropy_loss, None, train_loader, optimizer_model_softmax, epoch, device)
+
+        #print("Training Traditional with Laplace approximation...")
+        #train(model_softmax, cross_entropy_loss, None, train_loader, optimizer_model_softmax, epoch, device, laplace=True)
 
         ### RESHUFFLE FOR NEXT EPOCH ###
         train_loader = reshuffle_train(train_data)
 
+
+    print("Fitting Laplace approximation...") # VERY SLOW
+    la = Laplace(model_softmax, 'classification',
+         subset_of_weights='last_layer',
+         hessian_structure='diag')
+    la.fit(train_loader)
+    la.optimize_prior_precision(method='marglik')
+
     ### PLOTTING TRAINING EMBEDDINGS ###
-    train_embeddings, train_labels = get_all_embeddings(train_data, model)
-    plot_umap(f"train_{epoch}", train_embeddings, train_labels)
+    #train_embeddings, train_labels = get_all_embeddings(train_data, model)
+    #plot_umap(f"train_{epoch}", train_embeddings, train_labels)
 
 
     ### TESTING AND PLOTTING TEST EMBEDDINGS ###
-    print("Testing without Laplace approximation...")
-    test(model, train_loader, device, laplace=False)
-    test_embeddings, test_labels = get_all_embeddings(test_data, model)
-    plot_umap(f"test_{epoch}", test_embeddings, test_labels)
+    #print("Testing Siamese without Laplace approximation...")
+    #test_embeddings, test_labels = get_all_embeddings(test_data, model)
+    #plot_umap(f"test_{epoch}", test_embeddings, test_labels)
+
+    print("Testing Traditional without Laplace approximation...")
+    test(model_softmax, test_loader, device, False)
     
     print("Testing with Laplace approximation...")
-    # TODO: implement
+    test(la, test_loader, device, True)
 
     ### ACCURACY WITH KNN ###
-    print("Computing accuracy...")
-    train_labels = train_labels.squeeze(1)
-    test_labels = test_labels.squeeze(1)
-    test_acc = knn(train_embeddings, train_labels, test_embeddings, test_labels)
+    #print("Computing accuracy...")
+    #train_labels = train_labels.squeeze(1)
+    #test_labels = test_labels.squeeze(1)
+    #test_acc = knn(train_embeddings, train_labels, test_embeddings, test_labels)
     
-    print(f"Test set accuracy with KNN: {test_acc}")
-    wandb.log({"Embeddigns KNN Accuracy": test_acc})
+    #print(f"Test set accuracy with KNN: {test_acc}")
+    #wandb.log({"Embeddings KNN Accuracy": test_acc})
 
 if __name__ == "__main__":
 
--- src/models/model.py
+++ src/models/model.py
@@ -11,7 +11,7 @@ class Net(nn.Module):
         self.dropout2 = nn.Dropout2d(0.5)
         self.fc1 = nn.Linear(9216, 256)
         self.fc2 = nn.Linear(256, 64)
-        self.fc3 = nn.Linear(64,10)
+        self.fc3 = nn.Linear(64,32)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -27,3 +27,24 @@ class Net(nn.Module):
         x = self.dropout1(x)
         x = self.fc3(x)
         return x
+
+
+class NetSoftmax(nn.Module):
+    def __init__(self):
+        super(NetSoftmax, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.dropout1 = nn.Dropout2d(0.25)
+        self.dropout2 = nn.Dropout2d(0.5)
+        self.fc1 = nn.Linear(9216, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.dropout1(x)
+        x = torch.flatten(x, 1)
+        x = self.fc1(x)
+        return x#torch.softmax(x, dim=-1)
